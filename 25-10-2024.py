f687ead71b737920af36ca85cc62cf1fb21c1378
anilkumar <anilkumar631845@gmail.com>
2024-10-28 08:52:12 +0530

    Task added by anil.a

---
+++ 25-10-2024.py
@@ -0,0 +1,22 @@
+
+
+#packages 
+#packing means collection of related modules that provide specific functionality. t
+# hey help organise and structure code making easier to reuse and maintain.
+ 
+
+#module1
+
+def add(a,b):
+    return (a+b)
+def substract(a,b):
+    return(a-b)
+
+
+#module2 
+
+import  module1
+
+print(module1.add(10,5))
+print(module1.substract(10,45)) 
+
